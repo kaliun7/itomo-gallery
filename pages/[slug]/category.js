dcccf6a2cddbf5cd335b2f9f49ffc0cd2d5daaf9
zahari00 <i@zaharystoyanov.com>
2021-06-18 14:08:19 +0300

    checkmate

--- pages/[slug]/category.js
+++ pages/[slug]/category.js
@@ -1,7 +1,62 @@
-import { useRouter } from "next/router";
+import Link from "next/link";
 
-export default function CategoryPage() {
-  const router = useRouter();
+function CategoryPage({ category }) {
+  if (!category) {
+    return <h1>404 category not found</h1>;
+  }
 
-  return <h1>Category: {router.query.slug}</h1>;
+  // check the browser console
+  console.log("Category", category);
+
+  return (
+    <div>
+      <h1>
+        <img width={500} src={category.img} />
+      </h1>
+      All Products in this category:
+      <ul>
+        {category.products.map((product) => {
+          return (
+            <li key={product.slug}>
+              <Link href={`/${product.slug}/product`}>
+                <a>{product.name}</a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
+
+/**
+ * Find the page by slug
+ */
+CategoryPage.getInitialProps = ({ query }) => {
+  // get the array with the categories
+  const categories = require("../../public/categories.json");
+  const products = require("../../public/products.json");
+
+  const category = categories.find(
+    // check if the current product's slug
+    // matches the slug in the query
+    (currentCategory) => currentCategory.slug.trim() === query.slug.trim()
+  );
+
+  if (!category) return {};
+
+  // pass the category to the page
+  return {
+    category: {
+      ...category,
+      products: products.filter(
+        (product) => minify(product.category) === minify(category.id)
+      ),
+      img: `/images/category-images/${category.img.trim()}`,
+    },
+  };
+};
+
+const minify = (str) => str.trim().toLowerCase();
+
+export default CategoryPage;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,32 @@
 // only for the HOMEPAGE
 import styles from "../styles/Home.module.css";
-
-function Home() {
+import Link from "next/link";
+function Home({ categories }) {
   console.log(styles);
 
   return (
     <div className={styles.asdasd}>
       <h1>Homepage</h1>
+      <p>All categories</p>
+      <ul>
+        {categories.map((category) => {
+          return (
+            <li key={category.slug}>
+              <Link href={`/${category.slug}/category`}>
+                <a>{category.name}</a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
+
+Home.getInitialProps = () => {
+  return {
+    categories: require("../public/categories.json"),
+  };
+};
 
 export default Home;
